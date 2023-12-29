f10718c9560f2dbcee33b9bb8af86d74aafa1218
owengab <owengan@skyscanner.net>
2023-12-29 16:38:42 +0800

    Login page setup

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 // imports
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3001
 
+// Static Files
+app.use(express.static('public'))
+
 // listen on port 3001
 app.listen(port, () => console.info(`Running Express Server on port ${port}`))
+
+// routes
+
+app.get("",(req, res) => {
+    res.sendFile(__dirname + "/views/login.html")
+})
+
